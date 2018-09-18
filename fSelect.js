a6abe4ab7e4c026b15ad72eafbbc1dd0f0b1d539
Milan Ricoul <mricoul@beapi.fr>
2018-09-18 12:55:56 +0200

    #16 add accented and no accented keywords match

--- fSelect.js
+++ fSelect.js
@@ -1,5 +1,25 @@
 (function($) {
 
+    String.prototype.unaccented = function(){
+        var accent = [
+            /[\300-\306]/g, /[\340-\346]/g, // A, a
+            /[\310-\313]/g, /[\350-\353]/g, // E, e
+            /[\314-\317]/g, /[\354-\357]/g, // I, i
+            /[\322-\330]/g, /[\362-\370]/g, // O, o
+            /[\331-\334]/g, /[\371-\374]/g, // U, u
+            /[\321]/g, /[\361]/g, // N, n
+            /[\307]/g, /[\347]/g, // C, c
+        ];
+        var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+
+        var str = this;
+        for(var i = 0; i < accent.length; i++){
+            str = str.replace(accent[i], noaccent[i]);
+        }
+
+        return str;
+    }
+
     $.fn.fSelect = function(options) {
 
         if ('string' === typeof options) {
@@ -225,8 +245,10 @@
 
         if ('' != keywords) {
             $wrap.find('.fs-option').each(function() {
-                var regex = new RegExp(keywords, 'gi');
-                if (null === $(this).find('.fs-option-label').text().match(regex)) {
+                var regex = new RegExp(keywords.unaccented(), 'gi');
+                var formatedValue = $(this).find('.fs-option-label').text().unaccented()
+
+                if (null === formatedValue.match(regex)) {
                     $(this).addClass('hidden');
                 }
             });
